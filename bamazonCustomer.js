93d47af73e8f60515e1c79140cf131a33bbdd024
Leslie Moses <LesMoses@gmail.com>
2017-04-18 14:26:14 -0400

    bam 0418 230p

---
+++ bamazonCustomer.js
@@ -0,0 +1,35 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "root",
+    database: "bamazon"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    console.log("Welcome to Bamazon! Thanks for stopping by.");
+});
+// res = result
+// First create code that prints all songs within your database to the terminal.
+connection.query("SELECT * FROM products", function(err, res) {
+
+    for (var i = 0; i < res.length; i++) {
+        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price);
+    }
+    console.log("-----------------------------------");
+});
+
+// connection.query("SELECT * FROM songs WHERE genre=?", ["Dance"], function(err, res) {
+//     for (var i = 0; i < res.length; i++) {
+//         console.log(res[i].id + " | " + res[i].title + " | " + res[i].artist + " | " + res[i].genre);
+//     }
+// });
